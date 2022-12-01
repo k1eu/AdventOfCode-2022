ebd6f7c9675d0f4493ef4d76d3d1c339692e694a
Tomasz Kielar <git@k1eu.dev>
2022-12-01 21:09:20 +0100

    feat: add 01/b answer

--- 01/a.ts
+++ 01/a.ts
@@ -1,13 +1,20 @@
+//a
 const text = await Deno.readTextFile("./input.txt");
 
 const elvesItems = text.split("\n\n").map((item) => item.split("\n"));
 const elvesItemSummed = elvesItems.map((item) =>
   item.reduce((a, b) => a + parseInt(b), 0)
 );
-const highestCalories = elvesItemSummed.sort((a, b) => b-a)[0];
-const lowestCalories = elvesItemSummed.sort((a, b) => a-b)[0];
+const highestCalories = elvesItemSummed.sort((a, b) => b-a);
+// const lowestCalories = elvesItemSummed.sort((a, b) => a-b)[0];
 
-console.log({ elvesItems });
-console.log({ elvesItemSummed });
-console.log({ highestCalories });
-console.log({ lowestCalories });
+const highestCalorieElf = highestCalories[0]
+
+console.log({ highestCalorieElf });
+
+//b
+
+const topThree = highestCalories.slice(0, 3);
+const topThreesum = topThree.reduce((a, b) => a + b, 0);
+
+console.log({ topThreesum });
